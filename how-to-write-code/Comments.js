e67f280951c47e611224740302e3b2e5a45aace3
CodingL13 <lorechaser3@gmail.com>
2024-06-28 01:06:40 -0400

    parts 1 2 3

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -3,7 +3,7 @@
    // console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
-
+   console.log("I am King");
    /* Here is how
    to have
    multi-line
